becb83e7c2481eb6844bbef3040fbc4f4b539bd2
mthp <58376312+methamphetaminelab@users.noreply.github.com>
2024-10-27 09:45:33 +0300

    Delete app directory

--- app/keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-# Создаем кнопку "Чат с AI"
-keyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Чат с AI")]
-],
-resize_keyboard=True)
